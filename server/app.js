10b927c32848d8177e55ef1ec855e543872da6f4
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-12-21 19:33:23 -0500

    feat: renamed /js to /server; added /public as static asset in app.js; styled index.ejs file

--- server/app.js
+++ server/app.js
@@ -7,10 +7,11 @@ const app = express();
 const _filename = fileURLToPath(import.meta.url);
 const _dirname = path.dirname(_filename);
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(_dirname, "../views"));
 app.set("view engine", "ejs");
-
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 
